d624c4fcaea2f30aa3013875a5710d9359e6f7ae
Arshiya Noorpoor <archjanurpur@proton.me>
2024-09-12 14:48:11 +0330

    Errors fixed!

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,25 @@
 async function get_projects(){
     let y_d = await fetch("https://api.github.com/users/kaiserarchja/repos");
     let y = await y_d.json();
+    let ans = "";
     for(i in y){
-    document.getElementsByClassName("projects")[0].innerHTML += "<li><a href=\""+y[i]["html_url"]+"\">" + y[i]["name"]+ "</a></li>" + " <br>";
+    ans += "<li><a href=\""+y[i]["html_url"]+"\">" + y[i]["name"]+ "</a></li>" + " <br>";
 
     }
+    document.getElementById("projects").innerHTML = ans;
 }
 async function get_news() {
     let news = await fetch("https://api.github.com/users/kaiserarchja/events");
     let jsNews = await news.json();
-    for(i in jsNews){
-        let times = jsNews[i]["created_at"].split(/T|Z/);
-        let repoName = jsNews[i]["repo"]["name"].split('/');
+    let ans= "";
+    for(let i = 0; i < 15; i++){
+        let times = toString(jsNews[i]["created_at"]).split(/T|Z/);
+        let repoName = toString(jsNews[i]["repo"]["name"]).split("/");
         let url = jsNews[i]["repo"]["name"];
         let message = jsNews[i]["payload"]["commits"][0]["message"];
         let author = jsNews[i]["payload"]["commits"][0]["author"]["name"];
-        document.getElementsByClassName("newsData")[0].innerHTML += "<li>"+"<h6>---"+times[0]+"---"+times[1]+"---</h6>"+"<p>"+"Der Name des Repositorys:&nbsp &nbsp"+repoName[1]+"<br>"
-                                                                   +"Repo. URL:&nbsp &nbsp"+"<a href=\"https://github.com/"+url+"\">"+"Klicken Sie!"+"</a> <br>"
-                                                                   +"Nachricht:&nbsp &nbsp"+message+"<br>"
-                                                                   +"Autor:&nbsp &nbsp"+author+"</p>"+"</li>"
+        ans += "<li>"+"<h6>---"+times[0]+"---"+times[1]+"---</h6>"+"<p>"+"Der Name des Repositorys:&nbsp &nbsp"+repoName[1]+"<br>"+"Repo. URL:&nbsp &nbsp"+"<a href=\"https://github.com/"+url+"\">"+"Klicken Sie!"+"</a> <br>"+"Nachricht:&nbsp &nbsp"+message+"<br>"+"Autor:&nbsp &nbsp"+author+"</p>"+"</li> ";
     }
-    
+    document.getElementById("newsData").innerHTML = ans;
+
 }
-get_projects();
-get_news();
